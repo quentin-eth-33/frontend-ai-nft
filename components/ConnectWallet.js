c2daf385c07e455fc40e022b4e596ff3f55971bd
quentin-eth-33 <quentin.bret29@gmail.com>
2023-06-29 16:43:26 +0200

    update ethers version

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { ethers } from "ethers"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const ConnectWallet = () => {
   const [account, setAccount] = useState(null)
@@ -11,9 +11,24 @@ const ConnectWallet = () => {
       method: "eth_requestAccounts",
     })
     console.log("Accounts: ", accounts)
-    const address = ethers.getAddress(accounts[0])
+    const address = ethers.utils.getAddress(accounts[0])
     setAccount(address)
   }
+  useEffect(() => {
+    const checkSelectedAddress = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const network = await provider.getNetwork()
+      console.log("provider: ", provider)
+      console.log("network: ", network)
+
+      if (window.ethereum && window.ethereum.selectedAddress) {
+        const address = ethers.utils.getAddress(window.ethereum.selectedAddress)
+        setAccount(address)
+      }
+    }
+
+    checkSelectedAddress()
+  }, [])
 
   return (
     <nav>
