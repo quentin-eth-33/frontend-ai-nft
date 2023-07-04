0754fdbc5c64b278179cae106c52054a87e92c3a
quentin-eth-33 <quentin.bret29@gmail.com>
2023-07-04 15:42:49 +0200

    Connection API Hugging face

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -3,9 +3,18 @@
 import { ethers } from "ethers"
 import { useState, useEffect } from "react"
 import { contractAddresses, abi } from "../constants"
+import axios from "axios"
+import { Buffer } from "buffer"
+import Spinner from "react-bootstrap/Spinner"
+require("dotenv").config()
 
 const ConnectWallet = () => {
   const [account, setAccount] = useState(null)
+  const [message, setMessage] = useState("")
+  const [image, setImage] = useState(null)
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [isWaiting, setIsWaiting] = useState(false)
 
   const connectHandler = async () => {
     const accounts = await window.ethereum.request({
@@ -26,34 +35,100 @@ const ConnectWallet = () => {
     checkSelectedAddress()
   }, [])
 
-  const totalSupply = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const network = await provider.getNetwork()
-    console.log("provider: ", provider)
-    console.log("network: ", network)
-    console.log("address", contractAddresses.sepolia[0])
-    const nft = new ethers.Contract(contractAddresses.sepolia[0], abi, provider)
-    console.log("contract nft" + nft)
-    const signer = await provider.getSigner()
-    console.log("signer" + signer)
-    const transaction = await nft.connect(signer).totalSupply()
-    console.log("total supply: " + transaction)
+  const submitHandler = async (e) => {
+    e.preventDefault() // Empeche le rafraichissement de la page
+    const formData = {
+      name: name,
+      description: description,
+    }
+    setIsWaiting(true)
+    const imageData = await createImage()
+    console.log("imageData")
+    setIsWaiting(false)
+    setMessage("")
   }
+
+  const createImage = async () => {
+    setMessage("Generating Image...")
+    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`
+
+    const response = await axios({
+      url: URL,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer 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`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        inputs: description,
+        options: { wait_for_model: true },
+      }),
+      responseType: "arraybuffer",
+    })
+
+    const type = response.headers["content-type"]
+    const data = response.data
+
+    const base64data = Buffer.from(data).toString("base64")
+    const img = `data:${type};base64,` + base64data // <-- This is so we can render it on the page
+    setImage(img)
+
+    return data
+  }
+
   return (
-    <nav>
-      {account ? (
-        <button type="button" className="nav__connect">
-          {account.slice(0, 6) + "..." + account.slice(38, 42)}
-        </button>
-      ) : (
-        <button type="button" className="nav__connect" onClick={connectHandler}>
-          Connect
-        </button>
-      )}
-      <button type="button" className="nav__connect" onClick={totalSupply}>
-        Total Supply
-      </button>
-    </nav>
+    <div>
+      <nav>
+        {account ? (
+          <button type="button" className="nav__connect">
+            {account.slice(0, 6) + "..." + account.slice(38, 42)}
+          </button>
+        ) : (
+          <button type="button" className="nav__connect" onClick={connectHandler}>
+            Connect
+          </button>
+        )}
+      </nav>
+
+      <div className="image">
+        {!isWaiting && image ? (
+          <img src={image} alt="AI generated image" />
+        ) : isWaiting ? (
+          <div className="image__placeholder">
+            <Spinner animation="border" />
+            <p>{message}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Create a name..."
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Create a description..."
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input type="submit" value="Create & Mint" />
+        </form>
+      </div>
+      <style jsx>{`
+        .nav__connect {
+          position: absolute;
+          top: 0;
+          right: 0;
+        }
+      `}</style>
+    </div>
   )
 }
 
