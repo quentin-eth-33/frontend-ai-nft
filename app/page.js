eb398e08680c5aff1e800aade637b7c71cb770c8
quentin-eth-33 <quentin.bret29@gmail.com>
2023-07-04 16:17:39 +0200

    .

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import ConnectWallet from "../components/ConnectWallet"
+import FormCallApi from "../components/FormCallApi"
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
@@ -7,6 +8,9 @@ export default function Home() {
       </h1>
       <div className="absolute top-0 right-0 mt-8 mr-0">
         <ConnectWallet />
+      </div>
+      <div>
+        <FormCallApi />
       </div>
     </section>
   )
